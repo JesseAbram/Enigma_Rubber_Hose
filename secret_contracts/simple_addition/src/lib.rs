972b7878397932c3c3adf2d84f8cd7848ab96eb0
alicj <zichen.jiang@yahoo.com>
2019-11-10 03:32:54 -0500

    abstract write storage

--- secret_contracts/simple_addition/src/lib.rs
+++ secret_contracts/simple_addition/src/lib.rs
@@ -43,7 +43,27 @@ impl Contract {
     fn get_all_dummy_messages() -> Vec<DummyMessages> {
         read_state!(DUMMY_MESSAGES).unwrap_or_default()
     }
-   
+    fn store_dummy(message: String) {
+        //TODO JA: talk about security to recive this
+        // let owner: H160 = read_state!(OWNER).unwrap()
+        // assert_eq!(owner, sender)
+        let mut dummy_messages = get_all_dummy_messages();
+        dummy_messages.push(DummyMessages {
+            dummy_messages: message
+        });
+        write_state!(DUMMY_MESSAGES => dummy_messages);
+    }
+    fn store_secrets(accessor: H160, message: String) {
+        //TODO JA: talk about security to recive this
+        // let owner: H160 = read_state!(OWNER).unwrap()
+        // assert_eq!(owner, sender)
+        let mut secret_messages = get_all_secret_messages();
+        secret_messages.push(SecretMessages {
+            secret_messages: message,
+            secret_key: accessor
+        });
+        write_state!(SECRET_MESSAGES => secret_messages);
+    }
 }
 // For contract-exposed functions, declare such functions under the following public trait:
 #[pub_interface]
@@ -51,9 +71,14 @@ pub trait ContractInterface{
 
     fn construct(real_password: String, fake_password: String);
 
-    fn store_dummy(message: String);
+    // fn store_dummy(message: String);
 
-    fn store_secrets(accessor: H160, message: String);
+    // fn store_secrets(accessor: H160, message: String);
+
+    // pass in both password and message because front end does not know which store to call
+    // only contract knows
+    // TODO!!!
+    fn write_storage(password: String, message: String);
 
     fn read_storage(password: String) -> String; 
 
@@ -68,36 +93,18 @@ impl ContractInterface for Contract {
         write_state!(REAL_PASSWORD => real_password);
         write_state!(FAKE_PASSWORD => fake_password);
     }
-    
-    fn store_dummy(message: String) {
 
-        //TODO JA: talk about security to recive this
-        // let owner: H160 = read_state!(OWNER).unwrap()
-        // assert_eq!(owner, sender)
+    fn write_storage(password: String, message: String) {
+        let fake_password: String = read_state!(FAKE_PASSWORD).unwrap();
+        let real_password: String = read_state!(REAL_PASSWORD).unwrap();
 
-        let mut dummy_messages = Self::get_all_dummy_messages();
-
-        dummy_messages.push(DummyMessages {
-            dummy_messages: message
-        });
-
-        write_state!(DUMMY_MESSAGES => dummy_messages);
-    }
-
-    fn store_secrets(accessor: H160, message: String) {
-
-        //TODO JA: talk about security to recive this
-        // let owner: H160 = read_state!(OWNER).unwrap()
-        // assert_eq!(owner, sender)
-
-        let mut secret_messages = Self::get_all_secret_messages();
-
-        secret_messages.push(SecretMessages {
-            secret_messages: message,
-            secret_key: accessor
-        });
-
-        write_state!(SECRET_MESSAGES => secret_messages);
+        if password == fake_password {
+            Self::store_dummy(message);
+        } else if password == real_password {
+            Self::store_secret(message);
+        } else {
+            panic!("wrong password");
+        }
     }
 
     fn read_storage(password: String) -> String {
